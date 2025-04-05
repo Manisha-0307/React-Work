96c5dfffda6c0325ee4557b11720ded9bc002cfb
Manisha Kaushik <manishakaushik58@gmail.com>
2025-04-05 13:53:33 +0530

    chapter 2 changes

--- app.js
+++ app.js
@@ -13,6 +13,10 @@
 *
 */ 
 
+import React from 'react';
+import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client'
+
 const child1 = React.createElement('div', {id:"child1"}, [React.createElement('h1', {}, "Hello Heading from child1"),React.createElement('h2', {}, "Hello Sibling from child1")] )
 
 const child2 = React.createElement('div', {id:"child2"}, [React.createElement('h1', {}, "Hello Heading from child2"),React.createElement('h2', {}, "Hello Sibling from child2")] )
@@ -22,5 +26,5 @@ const parent = React.createElement('div', {id:"parent"},[child1, child2])
 
 
 // const heading = React.createElement("h1", {}, "Hello world from react");
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(parent);
